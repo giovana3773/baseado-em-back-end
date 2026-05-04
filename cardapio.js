7295793ed57298270c58ab31f22120e65ef511fa
giovana3773 <giovana.real@escola.pr.gov.br>
2026-05-04 11:53:02 +0000

    versao2

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,12 @@
+const cardapio = [
+    {nome: "X-Burguer", preco: 18.90, categoria:"lanche"},
+    {nome:"Batata-frita", preco: 9.50, categoria:"acompanhamento"},
+     {nome: "coca de vidro", preco: 7.90, categoria: "bebida"},
+     {nome: "sorvete", preco: 12.00, categoria:"sorbremesa"}
+];
 
+console.log(cardapio);
+
+const pedido = [];
+
+console.log(cardapio[0].nome);
