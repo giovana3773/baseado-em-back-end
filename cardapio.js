5e01ee7f2f39029df660edb7c18567a9658b01c8
giovana3773 <giovana.real@escola.pr.gov.br>
2026-05-05 11:53:14 +0000

    versao4

--- cardapio.js
+++ cardapio.js
@@ -10,3 +10,14 @@ console.log(cardapio);
 const pedido = [];
 
 console.log(cardapio[0].nome);
+console.log(cardapio[1].preco);
+console.log(cardapio[2].categoria);
+
+function exibirCardapio(lista) {
+    console.log("===CARDÁPIO===");
+    console.log(`0.${lista[0].nome} - R$ ${lista[0].preco}`)
+    console.log(`1. ${lista[1].nome} - R$ ${lista[1].preco}`)
+    console.log(`2. ${lista[2].nome} - R$ ${lista[2].preco}`)
+    console.log(`3. ${lista[3].nome} - R$ ${lista[3].preco}`)
+    console.log(`4. ${lista[3].nome} - R$ ${lista[4].preco}`)
+}
