15d5597efd929bb7aef2e99317d9e30631885f45
giovana3773 <giovana.real@escola.pr.gov.br>
2026-05-12 11:53:05 +0000

    versao2

---
+++ desafio.js
@@ -0,0 +1,8 @@
+// Cadastro de Aluno
+const aluno = {
+  nomeCompleto: "João Silva Sauro",
+  dataNascimento: "2010-05-15",
+  numeroMatricula: 2024001,
+  emailContato: "joao.silva@escola.com",
+  statusMatricula: "Ativo"
+};
