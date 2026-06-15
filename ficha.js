73e5f78227edb497ce9cb2cacec1879586e208a5
giovana3773 <giovana.real@escola.pr.gov.br>
2026-06-15 11:54:15 +0000

    versao2

--- ficha.js
+++ ficha.js
@@ -9,3 +9,19 @@ vacinado: false
 
 console.log(animal.nome);
 console.log(animal.tutor);
+
+const CAMISETA = {
+    tamanho: "G",
+    tecido: "Algodão",
+    design: true,
+    cor: "Preto",
+    marca: "Adidas"
+};
+
+
+console.log("--- Detalhes da Camiseta ---");
+console.log(`A camiseta da ${CAMISETA.marca} é feita de ${CAMISETA.tecido}`);
+console.log(`Cor: ${CAMISETA.cor}`);
+console.log(`Tamanho: ${CAMISETA.tamanho}`);
+console.log(`Tecido: ${CAMISETA.tecido}`);
+console.log(`Design: ${CAMISETA.design}`);
